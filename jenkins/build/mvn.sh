cd1795d99e756f2d3cb2cdea4430f5d0be4376f1
Sompong Rummaroj <ce16.engineering@gmail.com>
2022-09-15 20:46:28 +0700

    add platform linux/amd64 for run docker

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -11,4 +11,4 @@ WORKSPACE=/Users/sompong/jenkins_home/workspace/pipeline-docker-maven
 
 #docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 #docker run --rm  -v "$PWD/java-app":/app -v "$HOME/.m2":/root/.m2 -w /app maven "$@"
-docker run --rm --name maven-buid-jar  -v $WORKSPACE/java-app:/app -v /Users/sompong/.m2:/root/.m2 -w /app maven "$@"
+docker run --rm --name maven-buid-jar --platform linux/amd64  -v $WORKSPACE/java-app:/app -v /Users/sompong/.m2:/root/.m2 -w /app maven "$@"
